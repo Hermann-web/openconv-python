7dd9f6f70d13641988f4b9c48e5f18b23d323540
hermann-web <hermannagossou6@gmail.com>
2024-04-20 17:39:42 +0100

    activate env in docu build

--- scripts/run-build-docu.sh
+++ scripts/run-build-docu.sh
@@ -2,9 +2,14 @@
 
 poetry export -f requirements.txt --output ./docs/requirements.txt --without-hashes --with buildthedocs
 
-sphinx-apidoc ./openconv/ -o ./docs/source/openconv/ -f -E
+poetry run sphinx-apidoc ./openconv/ -o ./docs/source/openconv/ -f -E
+
+PYTHON_PATH="$(poetry env info --path)"
+source "$PYTHON_PATH/bin/activate"
 
 cd docs
 make html
 cd ..
+
+deactivate
 open docs/build/html/index.html
